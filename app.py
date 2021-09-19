32f03ea01815df5e26f006b2b20557331e4a45a1
MatanyaToren <matanya.to@gmail.com>
2021-09-19 15:35:33 +0300

    works, updating plots still needs some work

---
+++ app.py
@@ -0,0 +1,101 @@
+import numpy as np
+import scipy.signal as signal
+from threading import Thread
+from queue import Queue, Empty
+from tracking import *
+from welch_update import *
+from respiratory_rate import *
+
+
+class SampleError(RuntimeError):
+    def __init__(self):
+        self.message = "Couldn't get sample"
+
+
+class App():
+    def __init__(self, Fs=30, min_bpm=45, max_bpm=200):
+        
+        # queues to display results
+        self.SignalQueue = Queue()
+        self.WelchQueue = Queue()
+        self.RespQueue = Queue()
+    
+    
+        self.min_bpm, self.max_bpm = min_bpm, max_bpm
+        self.offset_x = 0.1
+        self.offset_y = 0.6
+        self.size_x = 0.2
+        self.size_y = 0.2
+        self.Fs = Fs
+        self.nperseg = 12 * Fs
+        self.noverlap = 10 * Fs
+        self.nstep = self.nperseg - self.noverlap
+        self.resp_nstep = 10 * Fs  # updating rate of the respiratory rate
+
+
+        self.HeartRate = 0
+        self.RespRate = 0
+        self.n = 0
+        self.raw_signal = []
+        self.filtered_signal = []
+        
+        self.bandPass = signal.firwin(100, np.array([min_bpm, max_bpm])/60, fs=Fs, pass_zero=False)
+        self.z = np.zeros(self.bandPass.shape[-1]-1)
+        
+        self.tracker = FaceTracker()
+        self.resp = respiratory(n_beats = 60, distance = int(2*Fs/3))
+        self.welch_obj = welch_update(fs=Fs, nperseg=self.nperseg, nwindows=20, nfft=Fs*60)
+
+           
+        
+    def new_sample(self, frame):
+        try:
+            self.bbox = self.tracker.update(frame)
+        
+        except (TrackingError, OutOfFrameError, DetectionError) as err:
+            raise SampleError
+           
+        self.get_signal(frame)
+        self.n += 1
+        
+        if 0 < self.n and 0 == self.n % 10:
+            filtered_chunk, self.z = signal.lfilter(self.bandPass, 1, self.raw_signal[-10:], zi=self.z)
+            self.filtered_signal = np.concatenate((self.filtered_signal, filtered_chunk))
+            self.SignalQueue.put(self.filtered_signal)
+
+        if self.nperseg < self.n and 0 == self.n % self.nstep:
+            f, pxx = self.welch_obj.update(self.filtered_signal[-self.nperseg:])
+            self.WelchQueue.put((f, pxx))
+            self.HeartRate = f[np.argmax(pxx)] * 60
+            # print(HeartRate)
+            # print(f.shape, f[np.argmax(pxx)])
+
+        if self.resp_nstep < self.n and 0 == self.n % self.resp_nstep:
+            # calculate the respiratory rate
+            freqs, pgram = self.resp.main(self.filtered_signal[-self.resp_nstep:])
+            self.RespQueue.put({'freqs': freqs, 'pgram': pgram, 'peak_times': np.array(self.resp.peak_times), 'rri':np.array(self.resp.rri)})
+            self.RespRate = freqs[pgram.argmax()] * self.Fs * 60
+
+             
+            
+    def get_signal(self, frame):
+        """
+        get signal from roi
+        """
+        x, y, w, h = self.bbox
+        
+        x_bb = int(x + self.offset_x * w)
+        w_bb = int(w * self.size_x)
+        y_bb = int(y + self.offset_y * h)
+        h_bb = int(h * self.size_y)
+        
+        self.roi = (x_bb, y_bb, w_bb, h_bb)
+        
+        # spatial mean of the bounding box of the face
+        self.raw_signal.append(np.mean(frame[x_bb:x_bb+w_bb, y_bb:y_bb+w_bb, 1][:]))
+        
+    
+    def quit(self):
+        pass
+    
+    
--- window.py
+++ window.py
@@ -9,47 +9,40 @@ from matplotlib.figure import Figure
 from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.backends.backend_qt5 import FigureCanvasQT
+from threading import Thread
+from queue import Queue, Empty
 from time import time
-from tracking import *
+from app import *
 
 
 class VideoThread(QThread):
     changePixmap = pyqtSignal(QImage)
     runs = True
-    tracker = FaceTracker()
     
-    def __init__(self, Fs=30, min_bpm=45, max_bpm=200):
+    def __init__(self, App):
         super().__init__()
-        
-        self.min_bpm, self.max_bpm = min_bpm, max_bpm
-        self.offset_x = 0.1
-        self.offset_y = 0.6
-        self.size_x = 0.2
-        self.size_y = 0.2
-
-        self.raw_signal = []
-        self.filtered_signal = []
-        
-        self.bandPass = signal.firwin(100, np.array([min_bpm, max_bpm])/60, fs=Fs, pass_zero=False)
-        self.z = np.zeros(self.bandPass.shape[-1]-1)
-        
+        self.App = App
 
     def run(self):
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        while self.runs:
+        # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        cap = cv2.VideoCapture('videos/56bpm_17_08.mp4')
+        while cap.isOpened() and self.runs:
             ret, frame = cap.read()
             if ret:
                 # https://stackoverflow.com/a/55468544/6622587
                 try:
-                    x, y, width, height = self.tracker.update(frame)
+                    self.App.new_sample(frame)
+                    x, y, width, height = self.App.bbox
+                    x_bb, y_bb, w_bb, h_bb = self.App.roi
                     cv2.rectangle(frame, (int(x), int(y)), (int(x+width), int(y+height)), (0, 255, 0), 2, 1)
+                    cv2.rectangle(frame, (x_bb, y_bb), (x_bb+w_bb, y_bb+h_bb), (0, 255, 0), 2)
                     frameRect =  cv2.flip(frame, 1)
-                    
-                    self.get_signal(frame, (x, y, width, height))
 
-                except (TrackingError, OutOfFrameError, DetectionError) as err:
+                except SampleError as err:
                     frameRect =  cv2.flip(frame, 1)
-                     
+                
+                cv2.putText(frameRect, "Heart Rate: {:.1f} bpm".format(self.App.HeartRate), (40,40), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+                cv2.putText(frameRect, "Breathing Rate: {:.1f} bpm".format(self.App.RespRate), (40,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)     
                 rgbImage = cv2.cvtColor(frameRect, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgbImage.shape
                 bytesPerLine = ch * w
@@ -57,27 +50,15 @@ class VideoThread(QThread):
                 p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
                 
-        cap.release()
+            else:
+                break
                 
+        cap.release()
+               
     def quit(self):
         self.runs = False
         
-    def get_signal(self, frame, bbox):
-        """
-        get signal from roi
-        """
-        x, y, w, h = bbox
-        
-        x_bb = int(x + self.offset_x * w)
-        w_bb = int(w * self.size_x)
-        y_bb = int(y + self.offset_y * h)
-        h_bb = int(h * self.size_y)
-        
-        self.roi = (x_bb, y_bb, w_bb, h_bb)
-        
-        # spatial mean of the bounding box of the face
-        self.raw_signal.append(np.mean(frame[x_bb:x_bb+w_bb, y_bb:y_bb+w_bb, 1][:]))
-
+    
 class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -97,13 +78,66 @@ class AppWindow(QWidget):
         self.label.setPixmap(QPixmap.fromImage(image))
         # print('frame displayed')
         
+    def WelchUpdate(self, n):
+        WelchLine, = self.WelchAx.get_lines()
+        
+        try:
+            f, pxx = self.App.WelchQueue.get_nowait()
+            WelchLine.set_data(f, pxx)
+            self.WelchAx.set_ylim([0, pxx.max()])
+            
+        except Empty:
+            pass
+        
+        return WelchLine, 
+        
+    def RespUpdate(self, n):
+        
+        ppgLine, maxLine = self.ppgAx.get_lines()
+        rriLine, = self.rriAx.get_lines()
+        lombLine, = self.lombAx.get_lines()
     
+        
+        try:
+            filtered_signal = self.App.SignalQueue.get_nowait()
+            N = len(filtered_signal)
+            currTime = N/self.App.Fs
+            t = np.linspace(start=0, stop=currTime, num=N, endpoint=False)
+            ppgLine.set_data(t, filtered_signal)
+            self.ppgAx.set_ylim([filtered_signal[-10*self.App.Fs:].min(), filtered_signal[-10*self.App.Fs:].max()])
+            self.ppgAx.set_xlim([currTime-10, currTime])
+        
+        except Empty:
+            pass
+        
+        try:
+            newData = self.App.RespQueue.get_nowait()
+            rri = newData['rri']/self.App.Fs
+            
+            filtered_signal = ppgLine.get_ydata()
+            t = ppgLine.get_xdata()
+            currTime = t[-1]
+            
+            maxLine.set_data(t[newData['peak_times']], filtered_signal[newData['peak_times']])
+            rriLine.set_data(t[newData['peak_times']], rri)
+            lombLine.set_data(newData['freqs'], newData['pgram'])
+            
+            self.rriAx.set_ylim([0, rri.max()])
+            self.rriAx.set_xlim([currTime-30, currTime])
+            
+            # self.lombAx.relim()
+            # self.lombAx.autoscale_view()
+            
+        except Empty:
+            pass
+        
+        return ppgLine, maxLine, rriLine, lombLine
     
     def closeEvent(self, event):
-        self.app.quit()
-        self.app.wait()
+        self.VideoSource.quit()
+        self.VideoSource.wait()
         event.accept()
-        print('closed window')
+    
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -117,12 +151,12 @@ class AppWindow(QWidget):
         self.label.resize(640, 480)
         
         # add figure for welch periodogram
-        self.WelchFig = Figure(figsize=(2,1)) # width, hight
+        self.WelchFig = Figure(figsize=(7,2)) # width, hight
         self.WelchCanvas = FigureCanvas(self.WelchFig)
         self.grid_layout.addWidget(self.WelchCanvas, 2, 0, 1, 2)
         
         # add figure for respiratory rate
-        self.RespFig = Figure(figsize=(4,9))
+        self.RespFig = Figure(figsize=(7, 9)) #(4,9)
         self.RespCanvas = FigureCanvas(self.RespFig)
         self.grid_layout.addWidget(self.RespCanvas, 0, 2, 3, 2)
         
@@ -133,25 +167,37 @@ class AppWindow(QWidget):
         self.WelchAx.set_xlabel('bpm')
         self.WelchAx.set_title('welch periodogram')
         self.WelchAx.set_xlim([0, 180])
+        self.WelchAx.set_ylim([0, 1])
         
         
         self.ppgAx, self.rriAx, self.lombAx = self.RespFig.subplots(nrows=3, ncols=1)
         self.ppgAx.plot([],[])
         self.ppgAx.plot([], [], "x")
-        self.ppgAx.set_xlabel('sample')
+        self.ppgAx.set_xlim([0, 60])
+        self.ppgAx.set_xlabel('time')
+        self.ppgAx.set_title('ppg signal')
 
         self.rriAx.plot([], [])
-        self.rriAx.set_xlabel('sample time')
+        self.rriAx.set_xlabel('time')
         self.rriAx.set_ylabel('rri')
+        self.rriAx.set_title('rri signal')
         
         self.lombAx.plot([], [])
         self.lombAx.set_xlabel('theta [rad]')
+        self.lombAx.set_title('lomb periogogram')
         
+        self.WelchFig.tight_layout()
         self.RespFig.tight_layout()
         
-        self.app = VideoThread()
-        self.app.changePixmap.connect(self.setImage)
-        self.app.start()
+        self.App = App()
+        
+        self.Welchani = FuncAnimation(self.RespFig, self.WelchUpdate, blit=False, interval=100) 
+        self.RespAni = FuncAnimation(self.RespFig, self.RespUpdate, blit=False, interval=100)
+        
+        self.VideoSource = VideoThread(self.App)
+        self.VideoSource.changePixmap.connect(self.setImage)
+        self.VideoSource.finished.connect(self.close)
+        self.VideoSource.start()
     
     
         self.show()
