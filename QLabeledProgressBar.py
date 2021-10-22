5a9137ff804efb00bea24013b4ad7d26603f88c7
MatanyaToren <matanya.to@gmail.com>
2021-10-22 12:05:19 +0300

    added color change to progress bar

--- QLabeledProgressBar.py
+++ QLabeledProgressBar.py
@@ -6,20 +6,16 @@ from PyQt5.QtCore import Qt
 
 
 class QLabeledProgressBar(QWidget):
-    def __init__(self, label='label for slider', *args, **kwargs):
+    def __init__(self, label='label for slider', range=(2,30), colormap={'red': (2,20), 'green': (20,30)}, *args, **kwargs):
         super().__init__()
         
+        self.colormap = colormap
         self.ProgressBar = QProgressBar(*args, **kwargs)
         self.ProgressBar.setOrientation(Qt.Vertical)
         self.ProgressBar.setAlignment(Qt.AlignCenter)
-        # self.ProgressBar.setGeometry(0,0,20,30)
-        self.ProgressBar.setValue(20)
-        self.ProgressBar.setStyleSheet("""QProgressBar::chunk
-                    {
-                    background-color: green;
-                    }""")
-        # self.snrLevelBar.setTextVisible(False)
-        self.ProgressBar.setFormat('hi')
+        self.ProgressBar.setRange(*range)
+        self.setValue(20)
+
         self.Label = QLabel()
         self.Label.setText(label)
         self.Label.setAlignment(Qt.AlignCenter)
@@ -31,9 +27,24 @@ class QLabeledProgressBar(QWidget):
         self.setLayout(self.Box)
         
         
+    def setValue(self, value):
+        for key, (min, max) in self.colormap.items():
+            if value >= min and value < max:
+                self.setColor(key)
+                self.ProgressBar.setValue(value)
+                self.ProgressBar.setFormat('{}'.format(value))
+            
+    def setColor(self, color):
+        self.ProgressBar.setStyleSheet(
+                    """QProgressBar::chunk{
+                    background-color: """ + color + """;
+                    }""")
+        
+        
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = QLabeledProgressBar()
+    ex = QLabeledProgressBar(label='snr\n[dB]')
+    ex.setValue(10)
     ex.show()
     sys.exit(app.exec_())
