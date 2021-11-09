1e1fa13153331903e2f9cfbd919588bb6c7092e9
MatanyaToren <matanya.to@gmail.com>
2021-11-09 21:33:01 +0200

    added graphs

--- procces_data.py
+++ procces_data.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy import signal
 from scipy.io import savemat, loadmat
-
+# %matplotlib tk
 # %%
 
-signal_mat = loadmat('output/signals_09_11_2021_12_21_57.mat')
+signal_mat = loadmat('output/signals_09_11_2021_12_21_57_hr_54.mat')
 
 for key, value in signal_mat.items():
     signal_mat[key] = np.squeeze(value)
@@ -14,7 +14,6 @@ for key, value in signal_mat.items():
 # %%
 raw = signal_mat['raw'] #[2000:-1100]
 filtered = signal_mat['filtered'] #[2000:-1100]
-print(raw.shape)
 t = np.linspace(0, raw.shape[0]/30, raw.shape[0])
 
 
@@ -91,20 +90,21 @@ plt.show()
 # %%
 # uneven sampling
 
-time = np.linspace(0, 10, 1000)
+time = np.linspace(0, 10, 100)
 x = np.sin(2*np.pi*time)
 
-ratio = 1/40
+ratio = 1/3
 samples = np.random.choice(x.shape[0], size=int(x.shape[0]*ratio), replace=False)
 samples.sort()
 
 lombFig, (xAx, fftAx, lombAx) = plt.subplots(3, 1)
 
-xAx.plot(time, x, label='signal')
-xAx.plot(time[samples], x[samples], 'x', label='samples')
+xAx.plot(time, x, 'o', fillstyle='none',color='#1f77b4', ms=4 ,label='even')
+xAx.plot(time[samples], x[samples], 'o', fillStyle='full', color='#1f77b4', ms=4, label='uneven')
 xAx.set_xlabel('time [sec]')
 xAx.set_title('$sin(2\pi\cdot t)$')
-# xAx.legend()
+xAx.set_ylim([-1.3, 1.3])
+xAx.legend(loc='lower right')
 xAx.grid(True)
 
 fftfreq = np.fft.fftfreq(int(x.shape[0]*ratio), d=10/int(x.shape[0]*ratio))
@@ -135,7 +135,7 @@ filtered_signal = signal_mat['filtered'][-58*30:]
 fft_signal = np.abs(np.fft.fft(filtered_signal))
 fftfreq_signal = np.fft.fftfreq(filtered_signal.shape[0], 1/30)
 
-WelchFftFig, (WelchSignalAx, fftSignalAx) = plt.subplots(2, 1, sharex=True)
+WelchFftFig, (fftSignalAx, WelchSignalAx) = plt.subplots(2, 1, sharex=True)
 
 WelchSignalAx.plot(signal_mat['f'], signal_mat['pxx'])
 WelchSignalAx.set_xlabel('frequency [Hz]')
@@ -154,3 +154,37 @@ plt.tight_layout()
 plt.show()
 
 
+
+# %%
+signal_mat1 = loadmat('output/signals_09_11_2021_16_38_28_far.mat')
+
+for key, value in signal_mat1.items():
+    signal_mat1[key] = np.squeeze(value)
+    
+
+signal_mat2 = loadmat('output/signals_09_11_2021_16_48_06_far.mat')
+
+for key, value in signal_mat2.items():
+    signal_mat2[key] = np.squeeze(value)
+    
+
+# %%
+FarNearFig, (nearAx, farAx) = plt.subplots(2, 1, sharex=True)
+
+nearAx.plot(signal_mat1['f'], signal_mat1['pxx'])
+nearAx.set_xlabel('frequency [Hz]')
+nearAx.set_title('Welch of signal from near, proximity indicator: 0.7')
+nearAx.xaxis.set_tick_params(labelbottom=True)
+nearAx.grid(True)
+
+farAx.plot(signal_mat2['f'], signal_mat2['pxx'])
+farAx.set_xlabel('frequency [Hz]')
+farAx.set_title('Welch of signal from far, proximity indicator: 0.1')
+farAx.xaxis.set_tick_params(labelbottom=True)
+farAx.grid(True)
+farAx.set_xlim([0.5, 3])
+
+plt.tight_layout()
+plt.show()
+
+# %%
